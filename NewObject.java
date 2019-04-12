ffa81e8f5b9987f65a5e5ff270d4332ce2b6702a
uncledear <991103897@qq.com>
2019-04-12 17:31:00 +0800

    this a test

---
+++ NewObject.java
@@ -0,0 +1,9 @@
+public class NewObject{
+    public static void main(String[] args){
+        People LiLei = new People();
+        LiLei.height=170;
+        LiLei.age=20;
+        LiLei.sex=1;
+        LiLei.printBasseMes();
+    }
+}
---
+++ People.java
@@ -0,0 +1,22 @@
+public class People {
+    double height;
+    int age;
+    int sex;
+
+    void cry(){
+        System.out.println("我在哭！");
+    }
+    void laugh(){
+        System.out.println("我在笑！");
+    }
+    void printBasseMes(){
+System.out.println("my height is "+height+"cm");
+
+    System.out.println("my age is "+age+"age");
+    if(this.sex==0)
+    System.out.println("i am a boy!");
+    else
+    System.out.println("i am a woman!");
+}
+
+}
