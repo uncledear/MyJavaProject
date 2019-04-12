003180ea0cae977de554f67cadf925b4a5b562c9
uncledear <991103897@qq.com>
2019-04-12 17:45:29 +0800

    the next commit

---
+++ Animal.java
@@ -0,0 +1,6 @@
+public class Animal{
+    public int legNum;
+    public void bark() {
+        System.out.println("the animal are barking");
+    }
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+
+public class Test{
+
+public static void main(String[] args){
+    Dog a= new Dog();
+a.legNum=4;
+a.bark();
+
+}
+}
